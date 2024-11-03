13c8148e502a02e707854f6a53cff3f3a9fe386a
Akshat <www.akshatagarwal123klj@gmail.com>
2024-11-03 19:56:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -85,5 +85,5 @@ def summarize():
 
 if __name__ == '__main__':
     # Add host and port for local development
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
 
